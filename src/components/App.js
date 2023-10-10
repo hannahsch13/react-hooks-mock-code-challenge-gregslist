30750bfcfe2f19c4587aa8d246bb136525cd4b2a
Hannah Schwaeble <hannah.schwaeble@gmail.com>
2023-10-10 10:07:09 -0400

    1 core deliverable DONE

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,12 @@ import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+  const list = 'http://localhost:6001/listings'
+
   return (
     <div className="app">
       <Header />
-      <ListingsContainer />
+      <ListingsContainer list={list}/>
     </div>
   );
 }
--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,14 +1,32 @@
-import React from "react";
-// import ListingCard from "./ListingCard";
+import React, {useState, useEffect} from "react";
+import ListingCard from "./ListingCard";
 
-function ListingsContainer() {
+function ListingsContainer({list}) {
+  const [idata,setData]=useState([])
+  useEffect(()=> {
+    const fetchData=async ()=>{
+     try {
+      const response = await fetch(list); 
+      const data= await response.json(); 
+      setData(data);
+    }catch(error) {
+      console.error('error',error);}}
+      fetchData();
+    })
+      
+
   return (
     <main>
       <ul className="cards">
-        {/* use the ListingCard component to display listings */}
+        {idata.map((list)=>{return <ListingCard key={list.id} image={list.image} description={list.description} location={list.location}/>})}
       </ul>
     </main>
   );
-}
+};
 
 export default ListingsContainer;
+
+
+// const [idata,setData]=useState([])
+// useEffect(()=>{const fetchData=async(()=>{try{const response= await fetch(list) 
+//   const data= await response.json() setData(data)}catch(error){console.error("error",error)}}) fetchData()
